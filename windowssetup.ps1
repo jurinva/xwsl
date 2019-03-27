122dfd6ed943a8d173f1f17b3b29181d6272ed58
jurinva <jurinva@gmail.com>
2019-03-27 13:16:45 +0300

    Fix

--- windowssetup.ps1
+++ windowssetup.ps1
@@ -1,16 +1,16 @@
 # Install X server
+# if you want to start all command in one line use semicolon as separator
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $path = Invoke-WebRequest "https://sourceforge.net/projects/xming/best_release.json" | ConvertFrom-Json | select -expand platform_releases | select -expand windows | select -ExpandProperty filename
 #e.g. $path = Xming/6.9.0.31/Xming-6-9-0-31-setup.exe
 Invoke-WebRequest -Uri https://datapacket.dl.sourceforge.net/project/xming/$path -OutFile $env:USERPROFILE\Downloads\Xming-setup.exe
 cd $env:USERPROFILE\Downloads
-Xming-setup.exe /silent /norestart
+.\Xming-setup.exe /silent /norestart
 C:\Program Files (x86)\Xming\XLaunch.exe
 
 # Enable WSL and restart
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+Enable-WindowsOptionalFeature -NoRestart -Online -FeatureName Microsoft-Windows-Subsystem-Linux
 restart-computer
 
 # Get Appx
 Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1604 -OutFile Ubuntu.appx -UseBasicParsing
-
-
